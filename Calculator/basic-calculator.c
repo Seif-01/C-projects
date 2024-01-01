f81f1c3eaa22cc7d84401311263adfdccfbe76d8
Seif-01 <sifreyassin@gmail.com>
2024-01-01 17:49:35 +0100

    Worked

--- Calculator/basic-calculator.c
+++ Calculator/basic-calculator.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main (){
-
+// input first number
 int num1;
 printf ("Enter first number\n");
 scanf ("%d", &num1);
+// input second number
 int num2;
 printf ("Enter second number\n");
 scanf ("%d", &num2);
+//display result
 int sum = num1 + num2 ;
 printf("the result is %d\n",sum);
     return 0;
