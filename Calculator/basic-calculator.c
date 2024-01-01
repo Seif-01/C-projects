1293349be9563f7b04f60d7b51edb1d1d5ab8a08
Seif-01 <sifreyassin@gmail.com>
2024-01-01 17:53:52 +0100

    edits here and there

--- Calculator/basic-calculator.c
+++ Calculator/basic-calculator.c
@@ -3,14 +3,14 @@
 int main (){
 // input first number
 int num1;
-printf ("Enter first number\n");
+printf ("Enter first number:\t");
 scanf ("%d", &num1);
 // input second number
 int num2;
-printf ("Enter second number\n");
+printf ("Enter second number:\t");
 scanf ("%d", &num2);
 //display result
 int sum = num1 + num2 ;
-printf("the result is %d\n",sum);
+printf("the result : %d\t",sum);
     return 0;
 }
