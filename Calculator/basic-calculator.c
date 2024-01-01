477f3d41c11db12848b99de7b6db8e000f78f407
Seif-01 <sifreyassin@gmail.com>
2024-01-01 17:45:46 +0100

    attempt 1

---
+++ Calculator/basic-calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+
+int num1;
+printf ("Enter first number\n");
+scanf ("%.2f", &num1);
+int num2;
+printf ("Enter first number\n");
+scanf ("%.2f", &num2);
+int sum = num1 + num2 ;
+printf("the result is %.3f\n",sum);
+    return 0;
+}
